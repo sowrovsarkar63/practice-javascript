aefccd1395de7c1e87c3b92fde6431ce8e5de024
sowrovsarkar63 <mhsstudio63@gmail.com>
2021-05-12 07:17:50 +0600

    remove comment frome some code

--- arrow.js
+++ arrow.js
@@ -9,15 +9,16 @@
 // // console.log(sum([1,2,3,45]));
 
 
-// const sum = (n) => {
-//     const summation =  n.reduce((o,n) => {
-//         return o + n
-//     })
-//     return summation
-// }
+const sum = (n) => {
+    const summation =  n.reduce((o,n) => {
+        return o + n
+    })
+    return summation
+}
 
-// console.log(sum([1,2,3]))
+console.log(sum([1,2,3]))
 
 const sum = (n) => n.reduce((o,n) =>o+n)
 
 console.log(sum([1,2,3]));
+
