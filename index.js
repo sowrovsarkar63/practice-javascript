108f67e44dbfbd99621041a17d3b75b20126553e
sowrovsarkar63 <mhsstudio63@gmail.com>
2021-05-12 07:07:51 +0600

    ALl essential js for react js and vue js here

---
+++ index.js
@@ -0,0 +1,112 @@
+/*const person = {fname:'John'};
+
+person.fname = 'md sowrov';
+person.lname = 'sarkar';
+person.profession = 'Software developer '
+
+
+console.log(person.fname);
+
+
+// constant with function 
+
+const RevStr = function(str){
+    return str.split('').reverse().join('');
+}
+
+console.log(RevStr('Hello world '));
+console.log(RevStr("sowrov"));
+*/
+
+
+// const p1 = 13
+// const p2 = 14
+
+// // const total = "Total price  = " + p1 + "+ " + p2 + " = " + (p1 + p2) 
+// const total = `Total price = ${p1} + ${p2} = ${p1+p2}`
+// console.log(total)
+
+// const fname = 'JOhn'
+// const lname = 'Doe'
+
+// const person ={fname:fname,lname:lname ,
+// introduce(){
+
+//     return(`Hi I'm ${person.fname} ${person.lname}`)
+
+// }
+// }
+
+// console.log(person.introduce())
+// const persones6 = {
+//     fname,
+//     lname,
+//     introduce(){ 
+//         return(`Hi I'm ${person.fname} ${person.lname}`)
+//     }
+
+
+// }
+
+// console.log(persones6.introduce());
+
+
+// function info(name ,...profession){
+//     return (`Hi I'm ${name} and i am a  ${profession}`)
+// }
+
+// console.log(info("sowrov sarkar","Full Stack Web Developer","Hacker", "Programmer"))
+// const person ={
+//     fname:"John",
+//     lname:"doe",
+//     age:27,
+//     sex:"M"
+
+// }
+
+// const {fname,lname,...rest} = person
+
+// console.log(fname,lname)
+// console.log(rest.age);
+
+
+// function getData() {
+//     const country = "Bangladesh"
+//     const city = "Rangpur"
+
+//     return {country , city}
+// }
+
+
+
+// const details = getData()
+// console.log(details);
+
+// const {country, city} = details
+// console.log(country,city);
+
+// const cities = ['Dhaka','Bogra','Khulna','Rangpur'];
+
+// const countries = {
+//     countryname:"Bangladesh",
+//     capital:"Dhaka"
+// }
+// for (city in cities) {
+    
+//     console.log(cities[city]);
+// }
+
+
+// const person ={
+//     fname:"jon",
+//     lname:"doe"
+
+// }
+
+
+// const anotherperson = Object.assign({}, person)
+
+//  anotherperson.fname = "Jane"
+//  console.log(anotherperson);
+
+//  console.log(person);
