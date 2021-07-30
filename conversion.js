11a50400918317c0a9c1a77fc0f4756e72ebdaed
Md sowrov sarkar <59117363+sowrovsarkar63@users.noreply.github.com>
2021-07-31 03:02:21 +0600

    JavaScript type conversion

---
+++ conversion.js
@@ -0,0 +1,13 @@
+var first = "Ariana";
+var last =  "grande";
+var name = first + ' ' + last;
+// console.log(name);
+
+
+var onionPrice = '42.50';//String 
+var eggPrice =  '10';//string 
+var onionPriceNumber = parseFloat(onionPrice);
+var eggPriceNumber = parseInt(eggPrice);
+// var totalPrice =;
+console.log( onionPriceNumber + eggPriceNumber)
+// console.log(eggPriceNumber);
