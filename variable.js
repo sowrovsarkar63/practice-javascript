fbd31882e327024698f094314f25e2b07792a29f
Md sowrov sarkar <59117363+sowrovsarkar63@users.noreply.github.com>
2021-07-31 02:11:06 +0600

    best practices

--- variable.js
+++ variable.js
@@ -1,10 +1,24 @@
+// newmerical
 var price =  21;
 var age = 13;
 var temperature = 37;
+
 // console.log(price);
+// String 
 var name  = 'md sowrov sarkar ';
 var address = "vooter goli kocu khet";
 var gf =  "alia bhatt";
 var school  = 'Ideal school and college';
+// Boolean 
 var pass =  true;
 var subscribed = false;
+
+
+// Variable best practice 
+
+var myEmailAddr =  "sowrovsarkar63@gmail.com";
+
+var 21price = 21; //invalid variable 
+var price21 = 21; //valid variable
+
+// 
