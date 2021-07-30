a698d951d6185154e624a7f833d4c5b795bac453
Md sowrov sarkar <59117363+sowrovsarkar63@users.noreply.github.com>
2021-07-31 02:26:53 +0600

    Simple mathmathics in javascript

---
+++ matheOperation.js
@@ -0,0 +1,16 @@
+var onionPrice = 43;
+var eggPrice =  10;
+var eggQuantity = 7 ;
+var eggPricieMultiplication = eggPrice * eggQuantity;
+// console.log(onionPrice);
+// console.log(eggPrice);
+
+
+var totalPrice = onionPrice + eggPrice;
+console.log("Total amount : " , totalPrice);
+
+var priceDifference = onionPrice -  eggPrice;
+console.log("Price diff : " ,priceDifference);
+
+var totalEggPrice =  eggPricieMultiplication;
+console.log("Total egg price : ", totalEggPrice);
