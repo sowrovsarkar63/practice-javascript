3007758c40e0f753041d054bfbb5e2402e6ece4f
Md sowrov sarkar <59117363+sowrovsarkar63@users.noreply.github.com>
2021-07-31 03:16:55 +0600

    JavaScript type conversion toFixed method

---
+++ variable-type.js
@@ -0,0 +1,12 @@
+var tShirtPrice =  200;
+console.log (typeof tShirtPrice);
+
+var isRaining = true;
+ console.log(typeof isRaining);
+ var isRomantice = null;
+ console.log(typeof isRomantice);
+
+ var firstNumber = 0.1;
+ var secondNumber =  0.2;
+ var total = firstNumber + secondNumber ;
+ console.log(parseFloat(total.toFixed(2)));
