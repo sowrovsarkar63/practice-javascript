5de439bcb52b86ce42f05792bcadd8043830b92f
Md sowrov sarkar <59117363+sowrovsarkar63@users.noreply.github.com>
2021-07-31 02:42:20 +0600

    Recap math in js

---
+++ math.js
@@ -0,0 +1,22 @@
+// Recaping 
+
+var price1 = 31;
+var price2 =  10;
+var sum = price1 + price2;
+var difference  =  price2 - price1;
+var multiplication = price1*price2;
+var division = price1 / price2 ;
+
+// console.log(sum);
+// console.log(difference);
+// console.log(multiplication);
+// console.log(division);
+
+var doublePrice =  price2 *2 ;
+//  price1 =  price1+10;
+price1 += 10;
+price2 -= 2;
+// 
+// console.log(doublePrice);
+// console.log(price1);
+console.log(price2);
