221d93cb13faf6386d0546ff55875ab065f05306
Md sowrov sarkar <59117363+sowrovsarkar63@users.noreply.github.com>
2021-08-02 00:09:30 +0600

    another practice day

---
+++ loop.js
@@ -0,0 +1,9 @@
+// Learn more about looping and become web developer (specialist skill would be it )
+var roastGiven = 0;
+
+while(roastGiven < 7 ){
+    console.log("Roast den, please!! ");
+    // roastGiven++;
+    console.log(roastGiven);
+
+}
---
+++ whileloop-example.js
@@ -0,0 +1,30 @@
+var number = 1;//loop variable 
+// while (number <= 10) {
+//     console.log(number);
+//     number++;
+// 
+// }
+// odd number 
+// while (number <= 20) {
+//     console.log(number);
+//     number +=2;
+// 
+// }
+
+// even number 
+
+// 
+// number = 0;
+// 
+// while (number <= 20) {
+//     console.log(number);
+//     number+=2;
+// }
+
+var number = [45,86,34,87,12,98,82];
+
+for (let i = 0; i < number.length; i++) {
+    var element = number[i];
+    console.log("Number is ",element);
+    
+}
