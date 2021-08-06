f78d761597ac9df1ca6bac941c44a61f4515193c
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2021-08-07 05:27:04 +0600

    Interest counter

---
+++ interestCounter.js
@@ -0,0 +1,22 @@
+/* 
+make a function , take a 
+
+// A	=	final amount
+P	=	initial principal balance
+r	=	annual interest rate shoud divided via 100.
+t	=	time (in years)
+as a parameter
+
+then Make take a variable and write the formula in js expression . then return it 
+
+
+*/
+
+function InterestCounter(PrincipalAmount, InterestRate, TimeFrameInYear) {
+  const Interest = (PrincipalAmount * InterestRate * TimeFrameInYear) / 100;
+
+  return "Final Interest is " + Interest;
+}
+
+const INTEREST = InterestCounter(10000, 5, 3);
+console.log(INTEREST);
