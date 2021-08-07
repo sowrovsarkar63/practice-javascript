e1c76421d975a0962dfd11fea62ad0dba893dc4c
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2021-08-07 06:38:11 +0600

    Km and mile problem solved

---
+++ kmTomile.js
@@ -0,0 +1,6 @@
+function KmToMile(mile) {
+  const Mile = mile / 1.609;
+  return Mile;
+}
+
+console.log(KmToMile(4));
---
+++ mileTokm.js
@@ -0,0 +1,6 @@
+function MileToKm(km) {
+  const Killometer = km * 1.60934;
+  return Killometer;
+}
+
+console.log(MileToKm(20));
