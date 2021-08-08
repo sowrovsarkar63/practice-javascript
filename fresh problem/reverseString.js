86beee9e178e7cb5d135262004f8a9966b8b48aa
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2021-08-08 07:32:07 +0600

    reverse a string

---
+++ fresh problem/reverseString.js
@@ -0,0 +1,18 @@
+let gretting = "Hello World ";
+
+// Reverse using prebuilt function
+
+console.log(gretting.split("").reverse().join("🔇"));
+/* 
+function reversString(str) {
+  let reversed = "";
+
+  for (const letter of str) {
+    reversed = letter + reversed;
+  }
+  return reversed;
+}
+
+const customizedString = reversString(gretting);
+console.log(customizedString);
+ */
