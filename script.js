2ef1204772510ca177221052a3dd23bc8128895f
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2021-08-13 13:41:58 +0600

    finished dom practice

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.dir(document);
+/* // Manuplate dom by html tag name
+const HeadingTag = document.getElementsByTagName("h3");
+for (const title of HeadingTag) {
+  console.log(title.innerText);
+}
+
+// Manupulate by id
+
+const header = document.getElementById("Header");
+header.style.color = "red";
+// header.style.textAlign = "center";
+header.style.margin = "0 auto";
+header.style.borderBottom = "2px solid ";
+header.style.borderWidth = "10px";
+header.style.maxWidth = "400px";
+header.setAttribute("title", "this is tooltip"); */
+
+// quesry selector ..
+
+// Create element
+
+let header = document.getElementById("Header");
+// create new element
+let h1 = document.createElement("h1");
+
+// Add some text
+header.innerText = "Blogs heading ";
+header.style.color = "red";
+header.style.fontSize = "25px";
+header.style.textAlign = "center";
+// append in the parent div
+header.appendChild(h1);
